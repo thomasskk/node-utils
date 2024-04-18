154886f464754b55439eaad7340c9ccb63d1effc
thomasskk <tkitengec@gmail.com>
2024-04-18 19:20:26 +0200

    chunk

--- lib/core/index.d.ts
+++ lib/core/index.d.ts
@@ -9,3 +9,4 @@ export declare const isDir: (path: string) => Promise<boolean>;
 export declare const execAsync: (cmd: string, options?: {
     log?: boolean;
 }) => Promise<void>;
+export declare const chunk: <T = unknown>(arr: T[], n: number) => T[][];
