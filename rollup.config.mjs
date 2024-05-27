242d6e224d31e664db4f672edab951d79157bf02
thomasskk <tkitengec@gmail.com>
2024-05-27 15:59:19 +0200

    feat(package): modify imports path

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,29 @@
-import terser from '@rollup/plugin-terser'
-import typescript from '@rollup/plugin-typescript'
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import fs from "node:fs";
+
+const dir = await fs.promises.readdir("src");
+const input = dir.reduce((input, file) => {
+	input[file] = `src/${file}/index.ts`;
+	return input;
+}, {});
 
 /** @type {import("rollup").RollupOptions} */
 export default {
-  input: 'src/index.ts',
-  plugins: [
-    typescript({
-      tsconfig: 'tsconfig.build.json',
-      outDir: 'lib',
-      rootDir: 'src',
-    }),
-    terser(),
-  ],
-  output: [
-    {
-      format: 'esm',
-      file: 'lib/index.mjs',
-    },
-  ],
-}
+	input,
+	plugins: [
+		typescript({
+			tsconfig: "tsconfig.build.json",
+			outDir: "lib",
+			rootDir: "src",
+		}),
+		terser(),
+	],
+	output: [
+		{
+			format: "esm",
+			dir: "lib",
+			entryFileNames: "[name]/index.mjs",
+		},
+	],
+};
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./xml2js/index.js";
-export * from "./sizeof/index.js";
-export * from "./core/index.js";
-export * from "./debug/index.js";
-export * from "./colors/index.js";
-export * from "./pretty-error/index.js";
-export * from "./dotenv/index.js";
